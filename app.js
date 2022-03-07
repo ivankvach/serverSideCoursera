e7cab18fedfee4fe7fa27acb3032bb2877289250
ivankvach <ivankvach6@gmail.com>
2022-03-07 13:56:34 +0200

    File Upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leadersRouter = require('./routes/leadersRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 
 const mongoose = require('mongoose');
@@ -55,9 +56,10 @@ app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leadersRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leadersRouter);
+app.use('/imageUpload', uploadRouter);
 
 
 // catch 404 and forward to error handler
